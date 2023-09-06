4127851646f8b965125392a5d4aa99f7017c8bf3
Tushar-2820 <tusharmore863@gmail.com>
2023-09-07 00:01:30 +0530

    Added Question6.java

--- OOPS Solved Questions/Question5.java
+++ OOPS Solved Questions/Question5.java
@@ -43,22 +43,21 @@ class Question5 {
 
 }
 
-class Main {
+// class Main {
 
-    public static void main(String[] args) {
-        Question5 s1 = new Question5("Tushar", "A");
-        Question5 s2 = new Question5("Shambhu", "B");
+// public static void main(String[] args) {
+// Question5 s1 = new Question5("Tushar", "A");
+// Question5 s2 = new Question5("Shambhu", "B");
 
-        s1.addCourse("Java");
-        s1.addCourse("C++");
-        s1.addCourse("C");
-        s1.addCourse("Flutter");
+// s1.addCourse("Java");
+// s1.addCourse("C++");
+// s1.addCourse("C");
+// s1.addCourse("Flutter");
 
-        s1.getDetails();
+// s1.getDetails();
 
-        s1.removeCouse("Java");
-        s1.getDetails();
+// s1.removeCouse("Java");
 
-    }
+// }
 
-}
+// }
---
+++ OOPS Solved Questions/Question6.java
@@ -0,0 +1,58 @@
+//Write a Java program to create a class called "Library" with a collection of books and methods to add and remove books.
+
+import java.util.ArrayList;
+
+class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getBooktitle() {
+        return title;
+    }
+
+    public String getBookAuthor() {
+        return author;
+    }
+}
+
+class Library {
+
+    ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<Book>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public ArrayList<Book> getCollections() {
+        return books;
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book("Alice in Wonderland", "Lewis Carrol");
+        Book b2 = new Book("Adventures of Sherlock Holmes", " Arthur Conan Doyle");
+        Library l1 = new Library();
+        l1.addBook(b1);
+        l1.addBook(b2);
+        // System.out.println(l1.getCollections().get(1).getBooktitle());
+
+        l1.removeBook(b2);
+        System.out.println(l1.getCollections().get(0).getBooktitle());
+
+    }
+}
